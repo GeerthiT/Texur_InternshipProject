17454fa8ed102f2fc4feb805c1fe6f405fea1e6b
Isak Sahlstrand <42635890+Isse2111@users.noreply.github.com>
2024-03-28 13:46:48 +0100

    Added Styling Courses Admin

--- Lexicon/settings.py
+++ Lexicon/settings.py
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
     'Lexicon_App'
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -119,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'Lexicon_App' / 'static'
 
 
 MEDIA_ROOT = BASE_DIR / 'Lexicon_App/static/images'
